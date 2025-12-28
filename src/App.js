47bafe536be280f8869ffa0e822b9d452bb3c8e9
Pooja Gujwar <gujwarpooja@gmail.com>
2025-12-28 13:33:26 +0530

    Instagram story Feat

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import storiesData from "./stories"
 
 function App() {
+  const [index, setIndex] = useState(0)
+  const [currentStory, setCurrentStory] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    if(currentStory){
+      const timer = setTimeout(()=>{
+        goNext()
+      },5000)
+      return ()=>clearTimeout(timer)
+    }
+  },[index, currentStory])
+
+  console.log(storiesData)
+  const openStory = (i) => {
+    setIndex(i)
+    setCurrentStory(storiesData[i])
+  }
+  const goPrev = () => {
+    if (index > 0) {
+      setIndex(index - 1)
+      setCurrentStory(storiesData[index - 1])
+    }else{
+      closeStory()
+    }
+  }
+  const closeStory = () => {
+    setCurrentStory(null)
+  }
+  const goNext = () => {
+    if (index < storiesData.length - 1) {
+      setIndex(index + 1)
+      setCurrentStory(storiesData[index + 1])
+    } else {
+      closeStory()
+    }
+  }
+  const handleTap = (e) => {
+    console.log(e)
+    const x = e.clientX;
+    const width = window.innerWidth
+    console.log(width, x)
+    if (x < width / 2) {
+      goPrev()
+    } else {
+      goNext()
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Instagram story</h1>
+      <div className='story-list'>
+        
+          {storiesData.map((story, i) =>
+            <img key={i} src={story.image} alt='story'
+              onClick={() => openStory(i)} />
+          )}
+        
+      </div>
+      {currentStory &&<> 
+      <div onClick={handleTap} className='story-view'
+      >
+        <button className='btn-close'
+        onClick={(e)=>{
+          e.stopPropagation()
+          closeStory()
+          }}>X</button>
+        <img key={index} src={currentStory.image} alt='Current Story' 
+        className='story-img'
+        onLoad={()=>setLoading(false)} />
+        {loading && <p style={{color:"black",position:"absolute"}}>Loading...</p>}
+      </div>
+      </>}
     </div>
+
   );
 }
 
